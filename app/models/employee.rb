e73daf0559507000eb23b6f790ca759514a37342
Will Browning <wdbrwnng@memphis.edu>
2024-04-24 15:11:08 -0500

    Employee Validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ApplicationRecord
+  validates :fname, presence: true, length: { maximum: 15 }
+  validates :lname, presence: true, length: { maximum: 15 }
+  validates :ssn, presence: true, uniqueness: true, format: { with: /\A\d{9}\z/, message: "must be 9 digits" }
+  validates :salary, presence: true, numericality: { greater_than_or_equal_to: 10000}
 end
