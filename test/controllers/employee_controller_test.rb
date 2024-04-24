8f16bb6876da97ecde9c31e031c64c3e537e2688
Will Browning <wdbrwnng@memphis.edu>
2024-04-24 15:35:31 -0500

    Added order function for default organization

--- test/controllers/employee_controller_test.rb
+++ test/controllers/employee_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class EmployeeControllerTest < ActionDispatch::IntegrationTest
   def index
+    @employees = Employee.order(:lname)
     render :index
   end
 end
--- test/controllers/project_controller_test.rb
+++ test/controllers/project_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class ProjectControllerTest < ActionDispatch::IntegrationTest
   def index
+    @projects = Project.order(:pnum)
     render :index
   end
 end
