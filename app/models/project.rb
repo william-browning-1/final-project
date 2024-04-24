85aa5146fc309081d7502aae3c57a8a72df84e57
Will Browning <wdbrwnng@memphis.edu>
2024-04-24 15:14:04 -0500

    Project Validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,14 @@
 class Project < ApplicationRecord
+  validates :pnum, presence: true, uniqueness: true
+  validates :pstdate, presence: true
+  validates :penddate, presence: true
+  validate  :validate_end_date_after_start_date
+
+  private
+
+  def validate_end_date_after_start_date
+    return if pstdate.blank? || penddate.blank?
+
+    errors.add(:penddate, "must be after the start date") if penddate < pstdate
+  end
 end
