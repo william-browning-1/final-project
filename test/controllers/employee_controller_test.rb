7a36b8a39196c1eacbd22601687945db37bd28eb
Will Browning <wdbrwnng@memphis.edu>
2024-04-24 22:17:56 -0500

    Changing issues with controller and view names

--- test/controllers/employee_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class EmployeeControllerTest < ActionDispatch::IntegrationTest
-  def index
-    @employees = Employee.all
-    @employees = Employee.order(:lname)
-    render :index
-  end
-end
--- test/controllers/project_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class ProjectControllerTest < ActionDispatch::IntegrationTest
-  def index
-    @projects = Project.all
-    @projects = Project.order(:pnum)
-    render :index
-  end
-end
