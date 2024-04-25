bb3258414c1a6bae03fde923e855e3df01161fdc
Will Browning <wdbrwnng@memphis.edu>
2024-04-24 22:20:37 -0500

    Added to controllers, deleted unneccessary files

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,6 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.order(:lname)
+    render :index
+  end
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,6 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.order_by(:lname)
+    render :index
+  end
 end
