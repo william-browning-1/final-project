b1107168e49e9043ee255c65e43240290275da0d
Will Browning <wdbrwnng@memphis.edu>
2024-04-26 09:42:21 -0500

    Updated Project Controller for server-side sorting

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,11 @@
 class ProjectsController < ApplicationController
   def index
     @projects = Project.all
+    # Check if sorting parameter is present
+    if params[:sort].present?
+      # Sort projects based on the selected attribute
+      @projects = @projects.order(params[:sort])
+    end
     render :index
   end
 end
